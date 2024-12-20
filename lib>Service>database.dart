b8b5c5ce2d2a8ec123d0590d3f1a8ba73f4da516
vishwajeetVam2011 <loveuvishwajeetkumar@gmail.com>
2024-12-20 21:42:29 +0530

    Update lib>Service>database.dart

--- lib>Service>database.dart
+++ lib>Service>database.dart
@@ -1,1 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+class DataBaseMethod{
+  Future addStudent(Map<String, dynamic> studentInfoMap, String id) async{
+    return await FirebaseFirestore
+        .instance
+        .collection("Students")
+        .doc(id)
+        .set(studentInfoMap);
+  }
+  Future<Stream<QuerySnapshot>> getStudentDetails()async{
+    return await FirebaseFirestore
+        .instance
+        .collection("Students")
+        .snapshots();
+  }
+
+  updateAttendence(String attendencecase, String id) async{
+    return await FirebaseFirestore
+        .instance
+        .collection("Students")
+        .doc(id)
+        .update({attendencecase : true }
+    );
+  }
+
+  deleteStudentData( String id) async{
+    return await FirebaseFirestore
+        .instance
+        .collection("Students")
+        .doc(id)
+        .delete();
+
+  }
+
+}
