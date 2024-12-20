4b9f56d55da70f41a2faeb9f84acb86e4801d467
vishwajeetVam2011 <loveuvishwajeetkumar@gmail.com>
2024-12-20 21:38:30 +0530

    Update home.dart

--- home.dart
+++ home.dart
@@ -1,1 +1,284 @@
+import 'package:attendence/Service/database.dart';
+import 'package:attendence/add_student.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key, required String title});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Stream? studentStream;
+
+  @override
+  void initState() {
+    super.initState();
+    loadStudentStream();
+  }
+
+  void loadStudentStream() async {
+    studentStream = await DataBaseMethod().getStudentDetails();
+    setState(() {});
+  }
+
+  Widget showStudentsList() {
+    return StreamBuilder(
+      stream: studentStream,
+      builder: (context, AsyncSnapshot snapshots) {
+        if (!snapshots.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshots.data.docs.isEmpty) {
+          return Center(child: Text("No Students Found!"));
+        }
+        return ListView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          itemCount: snapshots.data.docs.length,
+          itemBuilder: (context, index) {
+            DocumentSnapshot ds = snapshots.data.docs[index];
+            return Material(
+              elevation: 4,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "Name: ",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              ds["Name"] ?? "N/A",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            // Add delete functionality here
+                             DataBaseMethod().deleteStudentData(ds.id);
+                          },
+                          icon: Icon(Icons.delete),
+                          color: Colors.redAccent, // Professional and attractive color
+                          iconSize: 28, // Slightly smaller size for better appearance
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Text(
+                          "Roll Number: ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          ds["Roll_No"] ?? "N/A",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Text(
+                          "Age: ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          ds["Age"] ?? "N/A",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Text(
+                          "Attendance: ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        ds["Present"] == false
+                            ? GestureDetector(
+                          onTap: () async {
+                            await DataBaseMethod().updateAttendence("Present", ds.id);
+                          },
+                          child: Container(
+                            width: 38,
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "P",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                            : Container(
+                          width: 38,
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "P",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        ds["Absent"] == false
+                            ? GestureDetector(
+                          onTap: () async {
+                            await DataBaseMethod().updateAttendence("Absent", ds.id);
+                          },
+                          child: Container(
+                            width: 38,
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "A",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                            : Container(
+                          width: 38,
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "A",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => addstudents()));
+        },
+        child: Icon(Icons.add, color: Colors.white),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(top: 60, left: 20, right: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Student ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Attendance",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Expanded(child: showStudentsList()),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
